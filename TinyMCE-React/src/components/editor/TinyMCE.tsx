a71ab81ff93acabd43c0a4b7f7df13bf6cddb344
Htet Aung Lin <htetlin099798@gmail.com>
2024-03-20 19:01:32 +0630

    chore: config apiKey TinyMCE

--- TinyMCE-React/src/components/editor/TinyMCE.tsx
+++ TinyMCE-React/src/components/editor/TinyMCE.tsx
@@ -12,24 +12,22 @@ const TinyMCE = ({ apiKey }: { apiKey: string | undefined }) => {
 
   return (
     <Editor
-      apiKey={
-        process.env.TINYMCE_API_KEY ||
-        "4cp3boxi9yh8n0fns6z4nhyixpu1994aw1g4880glnlcedvj"
-      }
+      apiKey={apiKey}
       initialValue="<p>Tiny MCE</p>"
       init={{
         skin: "snow",
         icons: "thin",
+        toolbar_mode: "sliding",
         content_css: "document",
         setup: (editor) => {
           editor.ui.registry.addContextToolbar("tableoptions", {
             predicate: (node) => node.nodeName.toLowerCase() === "table",
-            items: "",
+            items: "tablemergecells tablecellbackgroundcolor",
             position: "node",
             scope: "node",
           });
         },
-        height: "100vh",
+        height: "110vh",
         menubar: true,
         plugins:
           "advlist autolink lists link image charmap print preview anchor searchreplace visualblocks code fullscreen textcolor insertdatetime media paste code help wordcount table",
